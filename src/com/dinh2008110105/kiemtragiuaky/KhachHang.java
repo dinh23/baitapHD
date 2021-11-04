72239dc7684802b0fe7cb101cf0488c1957e1ba6
dinh23 <emgai8653@gmail.com>
2021-11-04 12:31:01 +0700

    nop bai

---
+++ src/com/dinh2008110105/kiemtragiuaky/KhachHang.java
@@ -0,0 +1,29 @@
+package com.dinh2008110105.kiemtragiuaky;
+
+public class KhachHang extends Nguoi{
+    private String tenCongTy;
+    private double triGiaHoaDon;
+    public KhachHang (){
+        super();
+        setTenCongTy();
+        setTriGiaHoaDon();
+    }
+
+    public void setTriGiaHoaDon() {
+        do{
+            System.out.println("nhap gia tri hoa don");
+            triGiaHoaDon = n.nextDouble();
+        }while (triGiaHoaDon < 0 && triGiaHoaDon >10);
+    }
+
+    public void setTenCongTy() {
+        System.out.println("nhap ten cong ty: ");
+        tenCongTy = n.nextLine();
+    }
+
+   @Override
+   public String toString() {
+       return super.toString()+tenCongTy + " "+  triGiaHoaDon;
+   }
+}
+    
